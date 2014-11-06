1650c18d56f21f1f2a48d6f65d08ca9408dc1eeb
dyarob <dyarob@gmail.com>
2014-11-06 17:32:22 +0100

    neat css for teacher's students dashboard

--- views/teacher/dashboard.php
+++ views/teacher/dashboard.php
@@ -3,17 +3,25 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\LinkPager;
 ?>
+<link rel="stylesheet" type="text/css" href="css/new.css">
 <h1>Mes élèves</h1>
 <hr />
 <h3>Classe X</h3>
 <hr />
-<ul>
-	<?php foreach ($students as $student): ?>
-	<li>
-	<a href="<?= Url::base() ?>?r=teacher/student&first_name=<?= Html::encode("{$student->first_name}") ?>">
-	<?= Html::encode("{$student->first_name}") ?>
-	</a>
-	</li>
-	<?php endforeach; ?>
-</ul>
-<?= LinkPager::widget(['pagination' => $pagination]) ?>
+<div class="contents">
+<div class="column column-small">
+	<ul>
+		<?php foreach ($students as $student): ?>
+		<li>
+		<a href="<?= Url::base() ?>?r=teacher/student&first_name=<?= Html::encode("{$student->first_name}") ?>">
+		<?= Html::encode("{$student->first_name}") ?>
+		</a>
+		</li>
+		<?php endforeach; ?>
+	</ul>
+	<?= LinkPager::widget(['pagination' => $pagination]) ?>
+</div>
+<div class="column column-big">
+	coucou
+</div>
+</div>
